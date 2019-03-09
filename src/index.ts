0ff7aef48631a9f79367b808a059d98f28014249
Will Mitchell <wakamoleguy@gmail.com>
2019-03-08 16:49:53 -0800

    Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-const world = 'World'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const world = process.env.NAME || 'World'
 
 export function hello(word: string = world): string {
   return `Hello, ${word}!`
