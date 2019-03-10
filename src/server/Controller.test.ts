7cff66afe4fb649313e02193f40253717bfa5d75
Will Mitchell <wakamoleguy@gmail.com>
2019-03-10 16:48:53 -0700

    Ugly storage in controller for user actions

--- src/server/Controller.test.ts
+++ src/server/Controller.test.ts
@@ -1,26 +1,49 @@
+import MemoryUserStore from '../adapters/MemoryUserStore'
+import ConsoleAdapter from './ConsoleAdapter'
 import Controller from './Controller'
 
 describe('Controller', () => {
-  describe('create', () => {
-    it('echoes the interface adapter echo action', async () => {
-      const interfaceAdapter = {
-        on: jest.fn(),
-      }
-      Controller.create(interfaceAdapter)
-      expect(interfaceAdapter.on).toHaveBeenCalledWith(
-        'echo',
-        expect.any(Function)
-      )
+  it('echoes', async () => {
+    const interfaceAdapter = {
+      on: jest.fn(),
+    }
+    const store = MemoryUserStore
+    Controller.create(interfaceAdapter, store)
+    expect(interfaceAdapter.on).toHaveBeenCalledWith(
+      'echo',
+      expect.any(Function)
+    )
 
-      const callback = interfaceAdapter.on.mock.calls[0][1]
-      expect(await callback('Echo echo e...')).toBe('Echo echo e...')
-    })
+    const callback = interfaceAdapter.on.mock.calls[0][1]
+    expect(await callback('Echo echo e...')).toBe('Echo echo e...')
   })
 
-  /* What do I want it to do?
+  it('adds a user to the store', async () => {
+    const interfaceAdapter = new ConsoleAdapter()
+    const store = MemoryUserStore
+    Controller.create(interfaceAdapter, store)
+
+    const response = await interfaceAdapter.emit('addUser', 'wakamoleguy')
+    expect(response).toBe('ok')
+  })
+
+  it('lists all users in the store', async () => {
+    const interfaceAdapter = new ConsoleAdapter()
+    const store = MemoryUserStore
+    Controller.create(interfaceAdapter, store)
+
+    await interfaceAdapter.emit('addUser', 'wakamoleguy')
+    await interfaceAdapter.emit('addUser', 'will')
+    const response = await interfaceAdapter.emit('listUsers', '')
+    expect(response).toEqual(
+      JSON.stringify([{ id: 'wakamoleguy' }, { id: 'will' }])
+    )
+  })
+})
+
+/* What do I want it to do?
   1. Give it a store and a UI/API adapter
   2. It subscribes to UI changes?
   3. It performs actions and uses the store to persist them
   4. It selects objects from the store as needed.
   */
-})
--- src/server/Controller.ts
+++ src/server/Controller.ts
@@ -1,11 +1,45 @@
+import { StoreLibrary } from '../adapters/Store'
+import { User } from '../entities/User'
+
 export interface InterfaceAdapter {
   on(action: string, callback: (message: string) => Promise<string>): void
 }
 
-const Controller = {
-  create(interfaceAdapter: InterfaceAdapter) {
-    interfaceAdapter.on('echo', (message) => Promise.resolve(message))
-  },
+class Controller<Store> {
+  public static create<Store>(
+    interfaceAdapter: InterfaceAdapter,
+    store: StoreLibrary<Store, User>
+  ) {
+    return new Controller(interfaceAdapter, store)
+  }
+
+  private storeLib: StoreLibrary<Store, User>
+  private store: Store
+
+  constructor(
+    interfaceAdapter: InterfaceAdapter,
+    store: StoreLibrary<Store, User>
+  ) {
+    this.storeLib = store
+    this.store = store.empty()
+    interfaceAdapter.on('echo', this.echo)
+    interfaceAdapter.on('addUser', this.addUser)
+    interfaceAdapter.on('listUsers', this.listUsers)
+  }
+
+  private echo = (message: string) => {
+    return Promise.resolve(message)
+  }
+
+  private addUser = (userId: string) => {
+    const user: User = { id: userId }
+    this.store = this.storeLib.create(this.store, user)
+    return Promise.resolve('ok')
+  }
+
+  private listUsers = (_: string) => {
+    return Promise.resolve(JSON.stringify(this.storeLib.list(this.store)))
+  }
 }
 
 export default Controller
